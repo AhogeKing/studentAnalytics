bed6d37069ddb6e46c4644fa5f127f2b5e577d37
AhogeKing <leox986014@gmail.com>
2026-06-24 21:54:07 +0800

    还只是个空壳

--- src/main/java/com/ahogeking/studentanalytics/controller/PerformanceController.java
+++ src/main/java/com/ahogeking/studentanalytics/controller/PerformanceController.java
@@ -1,9 +1,16 @@
 package com.ahogeking.studentanalytics.controller;
 
+import com.ahogeking.studentanalytics.common.Result;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/student")
 public class PerformanceController {
+
+//    @GetMapping("/detail/{studentNo}")
+//    public Result
 }
